b99e9e1d7dc80ed06f8bd6b5504cf82d0d28fa06
Elena Litvinova <ivanovaev90@gmail.com>
2024-02-13 18:43:12 +0000

    Added data

--- components/ArtMenu/index.tsx
+++ components/ArtMenu/index.tsx
@@ -10,29 +10,98 @@ const links = [
         id: 1,
         href: '/',
         title: 'Blighted Badlands',
-        image: '/images/blighted_badlands.jpg',
+        image: '/images/badlands/preview.jpg',
         active: false,
+        modalInfo: {
+            title: 'Badlands',
+            content: (
+                <div>
+                    <p>Modern-day Dusktopia begins here</p>
+                    <p>The first thing that counts here is survival Only then can you think of profit; then finally domination</p>
+                    <p>Forge your alliances with the EON Corporation, the rival mafia gangs, or even an utterly corrupt police force - and crush all standing in your way</p>
+                </div>
+            ),
+            coverImage: '/images/badlands/modal_image.jpg',
+            listImages: [
+                '/images/badlands/list/badlands_01.png',
+                '/images/badlands/list/badlands_02.png',
+                '/images/badlands/list/badlands_03.png',
+                '/images/badlands/list/badlands_04.png',
+            ]
+        }
     },
     {
         id: 2,
         href: '/',
         title: 'Glacial Frontier',
-        image: '/images/glacial_frontier.jpg',
+        image: '/images/glacial_frontier/preview.jpg',
         active: false,
+        modalInfo: {
+            title: 'Glacial Frontier',
+            content: (
+                <div>
+                    <p>The home of everything warfare in Dusktopia</p>
+                    <p>Mercenaries and soldiers take payouts from the Cabal to do its bidding - but some want more for themselves, and are willing to go against power itself to get it</p>
+                    <p>Which side are you joining?</p>
+                </div>
+            ),
+            coverImage: '/images/glacial_frontier/modal_cover.jpg',
+            listImages: [
+                '/images/glacial_frontier/list/glacial_frontier_01.png',
+                '/images/glacial_frontier/list/glacial_frontier_02.png',
+                '/images/glacial_frontier/list/glacial_frontier_03.png',
+                '/images/glacial_frontier/list/glacial_frontier_04.png',
+            ]
+        }
     },
     {
         id: 3,
         href: '/',
         title: 'Sundered Grove',
-        image: '/images/sundered_grove.jpg',
+        image: '/images/sundered_grove/preview.jpg',
         active: false,
+        modalInfo: {
+            title: 'Glacial Frontier',
+            content: (
+                <div>
+                    <p>Dusktopia’s last remaining sanctuary of peace</p>
+                    <p>Researchers and scientists congregate here to live in religious pacifism, spending their time developing a renewable energy source for the world</p>
+                    <p>A small faction wants more - selling Aurium on the black market for their own personal gain</p>
+                    <p>Will you root out the traitors or join them?</p>
+                </div>
+            ),
+            coverImage: '/images/sundered_grove/modal_cover.jpg',
+            listImages: [
+                '/images/sundered_grove/list/sundered_grove_01.png',
+                '/images/sundered_grove/list/sundered_grove_02.png',
+                '/images/sundered_grove/list/sundered_grove_01.png',
+                '/images/sundered_grove/list/sundered_grove_02.png',
+            ]
+        }
     },
     {
         id: 4,
         href: '/',
         title: 'Sky Citadel',
-        image: '/images/sky_citadel.jpg',
+        image: '/images/sky_citadel/preview.jpg',
         active: false,
+        modalInfo: {
+            title: 'Sky Citadel',
+            content: (
+                <div>
+                    <p>These are where the apex predators in Dusktopia reside</p>
+                    <p>The political and economic masters of all society: living high above the suffering of ordinary denizens, and calmly plot elevated pursuits like space colonization in absolute peace</p>
+                    <p>If you’re lucky enough to be part of it, hold on tight - everybody wants what you have</p>
+                </div>
+            ),
+            coverImage: '/images/sky_citadel/modal_cover.jpg',
+            listImages: [
+                '/images/sky_citadel/list/sky_citadel.png',
+                '/images/sky_citadel/list/sky_citadel.png',
+                '/images/sky_citadel/list/sky_citadel.png',
+                '/images/sky_citadel/list/sky_citadel.png',
+            ]
+        }
     }
 ];
 
