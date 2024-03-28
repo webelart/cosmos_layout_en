8f6caed74e64b222f12e9f42c7b68ccf16dfa3c8
Elena Litvinova <hello@webelart.com>
2024-03-28 11:56:17 +0000

    Animation was added

--- components/AnimatedMenu/index.tsx
+++ components/AnimatedMenu/index.tsx
@@ -1,38 +1,45 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import cl from 'classnames';
 
 import styles from './style.module.scss';
 
-const links = [
+interface Item {
+    id: string;
+    title: string;
+    text?: string;
+    imageUrl: string;
+}
+
+const items: Item[] = [
     {
         id: "01",
         title: "Genesis lands drop",
         text: "An original collection of 5,555 land plots will be made ready for the very first Dusktopian adventurers.",
-        imageUrl: "./images/01.png",
+        imageUrl: "/animatedMenu/01.png",
     },
     {
         id: "02",
         title: "Staking goes live",
         text: "The EON Corporation Superbank begins rewarding Dusktopians staking their land plots with $DAWN.",
-        imageUrl: "./images/01.png",
+        imageUrl: "/animatedMenu/02.png",
     },
     {
         id: "03",
         title: "New worlds emerge",
         text: "Claim a slice of our secret land collection with $DAWN. Outfit your existing land with new properties and accessories.",
-        imageUrl: './images/01.png',
+        imageUrl: '/animatedMenu/03.png',
     },
     {
         id: "04",
         title: "Avatar collection drop",
         text: "Get the PFP avatars you will use for in-universe interaction, synergized with your asset holdings.",
-        imageUrl: './images/01.png',
+        imageUrl: '/animatedMenu/04.png',
 
     },
     {
         id: "05",
         title: "Let's the game begin",
-        imageUrl: './images/01.png',
+        imageUrl: '/animatedMenu/05.png',
     }
 ];
 
@@ -42,6 +49,42 @@ interface AnimatedMenuProps {
 
 export const AnimatedMenu: React.FC<AnimatedMenuProps> = ({
     className
-}) => (
-    <div>Animated Menu</div>
-);
+}) => {
+    const [ activeItem, setActiveItem ] = useState(0);
+
+    return (
+        <>
+            <div className={cl(styles.animatedMenu, className)}>
+                {items.map((item, index) => (
+                    <div
+                        key={item.id}
+                        className={cl(
+                            styles.animatedMenuItem,
+                            activeItem === index && styles.animatedMenuItemActive,
+                            activeItem === 0 && styles.animatedMenuItemPink
+                        )}
+                        onClick={() => setActiveItem(index)}
+                    >
+                        <div className={styles.animatedMenuItemContent}>
+                            <span className={styles.animatedMenuItemNumber}>{item.id}</span>
+                            <h4 className={styles.animatedMenuItemTitle}>{item.title}</h4>
+                            {item.text && (<p className={styles.animatedMenuItemText}>{item.text}</p>)}
+                            <span className={styles.animatedMenuDecorator} />
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.animatedImages}>
+                {items.map((item, index) => (
+                    <div
+                        key={item.id}
+                        style={{
+                            backgroundImage: `url('${item.imageUrl}')`
+                        }}
+                        className={cl(styles.animatedImagesItem, activeItem === index && styles.animatedImagesItemActive)}
+                    />
+                ))}
+            </div>
+        </>
+    );
+};
