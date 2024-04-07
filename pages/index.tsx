d454380b72f6cc3602a975faa75f5ce8f372e636
Elena Litvinova <hello@webelart.com>
2024-04-07 09:21:05 +0100

    Background

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,11 +70,26 @@ export default function Home() {
         </Title>
         <AnimatedMenu />
       </section>
-      <section className={cl(styles.section, styles.sectionDifficultForms)}>
+      <section className={cl(styles.section, styles.sectionLogos)}>
         <ArtTitle type="up" className={styles.artTitle}>Partners</ArtTitle>
         <Title size="X-Medium" className={styles.subTitle}>Collaborations</Title>
         <LogoList className={styles.logoList} />
-        <ArtObject />
+      </section>
+      <section className={cl(styles.section, styles.sectionDifficultForms)}>
+        <div className={styles.sectionContent}>
+          <ArtTitle type="up" className={styles.artTitle}>FAQ</ArtTitle>
+          <Title size="X-Medium" className={styles.subTitle}>
+            Have
+            <svg width="56" height="56" fill="none" className={styles.superStar}>
+              <g clipPath="url(#a)"><path fill="#fff" fillRule="evenodd" d="M30 1.12A1.12 1.12 0 0 0 28.88 0h-1.76A1.12 1.12 0 0 0 26 1.12v10.829c0 1.231-1.697 1.56-2.158.42l-4.057-10.04a1.12 1.12 0 0 0-1.458-.62l-1.632.66a1.12 1.12 0 0 0-.619 1.458l4.325 10.704c.457 1.13-.968 2.074-1.83 1.211L10.407 7.58a1.12 1.12 0 0 0-1.584 0L7.58 8.823a1.12 1.12 0 0 0 0 1.584l7.33 7.33c.875.876-.106 2.315-1.24 1.82l-9.5-4.151a1.12 1.12 0 0 0-1.475.577l-.704 1.613a1.12 1.12 0 0 0 .578 1.475l10.947 4.783c1.111.486.765 2.146-.449 2.146H1.12A1.12 1.12 0 0 0 0 27.12v1.76C0 29.499.501 30 1.12 30h11.946c1.214 0 1.56 1.66.448 2.146L2.568 36.93a1.12 1.12 0 0 0-.578 1.475l.704 1.612a1.12 1.12 0 0 0 1.475.578l9.5-4.15c1.134-.496 2.115.943 1.24 1.818l-7.33 7.33a1.12 1.12 0 0 0 0 1.585l1.244 1.244a1.12 1.12 0 0 0 1.584 0l8.163-8.163c.863-.863 2.288.08 1.83 1.211l-4.324 10.704a1.12 1.12 0 0 0 .62 1.459l1.63.659a1.12 1.12 0 0 0 1.459-.62l4.057-10.04c.46-1.141 2.158-.81 2.158.42V54.88c0 .618.501 1.12 1.12 1.12h1.76A1.12 1.12 0 0 0 30 54.88V44.051c0-1.23 1.697-1.561 2.159-.42l4.056 10.04c.232.575.884.851 1.458.62l1.632-.66a1.12 1.12 0 0 0 .619-1.458L35.599 41.47c-.457-1.13.968-2.074 1.83-1.211l8.164 8.163a1.12 1.12 0 0 0 1.584 0l1.244-1.244a1.12 1.12 0 0 0 0-1.584l-7.33-7.33c-.875-.876.106-2.315 1.24-1.819l9.5 4.15a1.12 1.12 0 0 0 1.475-.578l.704-1.612a1.12 1.12 0 0 0-.578-1.475l-10.946-4.783C41.374 31.66 41.72 30 42.934 30H54.88A1.12 1.12 0 0 0 56 28.88v-1.76A1.12 1.12 0 0 0 54.88 26H42.934c-1.214 0-1.56-1.66-.448-2.146l10.946-4.783a1.12 1.12 0 0 0 .578-1.475l-.704-1.613a1.12 1.12 0 0 0-1.475-.578l-9.5 4.151c-1.134.496-2.115-.943-1.24-1.818l7.33-7.33a1.12 1.12 0 0 0 0-1.585L47.177 7.58a1.12 1.12 0 0 0-1.584 0l-8.163 8.163c-.863.863-2.288-.08-1.83-1.212l4.324-10.703a1.12 1.12 0 0 0-.619-1.458l-1.632-.66a1.12 1.12 0 0 0-1.458.62l-4.056 10.04c-.462 1.14-2.159.811-2.159-.42z" clipRule="evenodd"/></g>
+              <defs><clipPath id="a"><path fill="#fff" d="M0 0h56v56H0z"/></clipPath></defs>
+            </svg>
+            any
+            <br />
+            questions?
+          </Title>
+          <ArtObject />
+        </div>
       </section>
     </article>
   )
