c17582f201067a6eecc013c272976542d7f15306
Elena Litvinova <hello@webelart.com>
2024-04-20 14:48:26 +0100

    Star was added

--- components/AlliesGallery/index.tsx
+++ components/AlliesGallery/index.tsx
@@ -113,6 +113,13 @@ export const AlliesGallery: React.FC<AlliesGalleryProps> = ({
                             <h4 className={styles.alliesTitle}>{item.title}</h4>
                         </div>
                         <div className={styles.alliesDescription}>
+                            <svg width="33" height="33" viewBox="0 0 33 33" fill="none" className={styles.alliesDescriptionStar}>
+                                <path
+                                    d="M32.0377 15.8232C32.7571 15.9876 32.7571 17.0124 32.0377 17.1768L20.3655 19.8433C20.1056 19.9027 19.9027 20.1056 19.8433 20.3655L17.1767 32.0377C17.0124 32.7571 15.9876 32.7571 15.8232 32.0377L13.1567 20.3655C13.0973 20.1056 12.8944 19.9027 12.6345 19.8433L0.962295 17.1767C0.242854 17.0124 0.242858 15.9876 0.962295 15.8232L12.6345 13.1567C12.8944 13.0973 13.0973 12.8944 13.1567 12.6345L15.8232 0.962294C15.9876 0.242855 17.0124 0.242856 17.1768 0.962295L19.8433 12.6345C19.9027 12.8944 20.1056 13.0973 20.3655 13.1567L32.0377 15.8232Z"
+                                    fill="white"
+                                />
+                            </svg>
+
                             <p dangerouslySetInnerHTML={{__html: item.description}} />
                         </div>
                     </div>
